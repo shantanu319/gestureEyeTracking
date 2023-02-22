d7aa5c9e8f98a7672b182d9e668f50ed6709a0aa
Shantanu Khaladkar <53188371+shantanu319@users.noreply.github.com>
2023-02-22 08:40:30 -0800

    outlining

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+1. initialize window, take webcam input 
+2. run gazetracking and gesture tracking in parallel
+
+https://github.com/luca-ant/gaze_tracker/blob/master/gt/main.py
+
+https://opencv.org/releases/
+
+https://www.youtube.com/watch?v=iLc8Xm3zILc
+
+"""
